ad26b78ee2fae2d7461634c9f40015ab8dbebcaf
Ariel Choquetopa <ariel.choquetopa@hotmail.com>
2025-03-10 20:08:20 -0400

    structure

---
+++ src/main/java/com/polarapss/cerebrinping/CerebrinpingApplication.java
@@ -0,0 +1,13 @@
+package com.polarapss.cerebrinping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerebrinpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerebrinpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polarapss/cerebrinping/CerebrinpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarapss.cerebrinping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerebrinpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
